ebc5305fd58f102d00c02670b53448de30a8cac6
Roohi Singh <singhroohi184@gmail.com>
2020-11-28 23:34:04 -0500

    updated
    updated

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
+/*jslint browser: true */
+/*globals $:false */
+/*global window */
 /*===================================
             SERVICES
 ===================================*/
@@ -5,4 +8,33 @@
 $(function () {
   //animate on
   new WOW().init();
+});
+
+/*===================================
+            WORK
+===================================*/
+
+$(function () {
+  $("#work").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
+
+/*===================================
+            NAVIGATION
+===================================*/
+//show/hide transparent black navigation
+
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < 50 ) {
+      $("nav").removeClass("pot-top-nav");
+    } else {
+      $("nav").addClass("pot-top-nav");
+    }
+  });
 });
